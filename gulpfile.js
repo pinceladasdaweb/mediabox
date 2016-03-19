16b8bca65e95189ff700e437ddbeff238bb6d0e4
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-03-18 23:38:57 -0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,34 @@ var gulp     = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     rename   = require('gulp-rename'),
     package  = require('./package.json'),
-    bannerJs,
-    bannerCss;
+    banner;
 
-banner = {
-    js: '/*! ' +
-        '<%= package.name %> ' +
-        'v<%= package.version %> | ' +
-        '(c) ' + new Date().getFullYear() + ' <%= package.author %> |' +
-        ' <%= package.homepage %>' +
-        ' */' +
-        '\n',
-    css: '/*\n' +
-        'Style By: Qassim Hassan\n' +
-        'Twitter: @QQQHZ\n' +
-        'Websites: wp-time.com | qass.im | wp-plugins.in\n' +
-        'Copyright (c) 2016 - Qassim Hassan\n' +
-        'Adapted by: <%= package.author %> for <%= package.name %> v<%= package.version %> | <%= package.homepage %> ' +
-        '*/\n'
-}
+banner = [
+  '/*! ',
+    '<%= package.name %> ',
+    'v<%= package.version %> | ',
+    '(c) ' + new Date().getFullYear() + ' <%= package.author %> |',
+    ' <%= package.homepage %>',
+  ' */',
+  '\n'
+].join('');
 
 gulp.task('compress:js', function() {
   return gulp.src('src/mediabox.js')
-    .pipe(header(banner.js, { package : package }))
+    .pipe(header(banner, { package : package }))
     .pipe(gulp.dest('dist/'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(header(banner.js, { package : package }))
+    .pipe(header(banner, { package : package }))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('compress:css', function() {
   return gulp.src('src/mediabox.css')
-    .pipe(header(banner.css, { package : package }))
+    .pipe(header(banner, { package : package }))
     .pipe(gulp.dest('dist/'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleanCSS())
-    .pipe(header(banner.css, { package : package }))
     .pipe(gulp.dest('dist/'));
 });
 
